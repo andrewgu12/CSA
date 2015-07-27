62c603db35b1d706a92ce66298c85a119f42c3c4
Andrew Gu <andrew@mingbogu.com>
2015-07-26 23:21:20 -0400

    add, delete, update events are done

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -13,14 +13,15 @@ class AdminController < ApplicationController
     def index
       @hide_menu = true
     end
+    def pictures_add
 
+    end
     def events_view
       @events = Event.all
     end
     def event_view
-      puts "Params: #{params[:id]}"
       @event = Event.find_by(event_id: params[:id])
-      print "Event: #{@event.name}"
+      @pictures = Picture.find_by(event_id: @event.id)
     end
     def event_new
     end
@@ -28,6 +29,29 @@ class AdminController < ApplicationController
       @event = Event.new(event_params)
       @event.event_id = @event.name.downcase.gsub(/\s/, "_")
       @event.save
+    end
+
+    def event_edit
+      @event = Event.find_by(event_id: params[:id])
+    end
+
+    def event_update
+      @event = Event.find_by(event_id: params[:id])
+      @event.update_attributes(event_params)
+      @event.event_id = @event.name.downcase.gsub(/\s/, "_")
+      @event.save
+    end
+
+    def event_delete
+      @event = Event.find_by(event_id: params[:id])
+      @event.destroy
+    end
+
+    def pictures_new
+      @events = Event.all
+    end
+    def pictures_create
+      @picture = Picture.new(picture_params)
     end
     private
 
@@ -41,5 +65,8 @@ class AdminController < ApplicationController
 
     def event_params
       params.require(:event).permit(:name, :date, :description)
+    end
+    def picture_params
+      params.require(:picture).permit(:event_id, :year, :link)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,12 +22,17 @@ Rails.application.routes.draw do
   get '/admin/ao/dashboard' => "admin#ao_dashboard"
   get '/admin' => 'admin#index'
   post '/admin' => 'admin#login'
-  get '/admin/events/all' => 'admin#events_view'
-  get '/admin/events/:id' => 'admin#event_view'
   # get '/admin/events/:id/edit' => 'admin#edit_event'
   # post '/admin/events/:id/edit' => 'admin#save_edited_event'
   get '/admin/events/add' => 'admin#event_new'
   post '/admin/events/add' => 'admin#event_create'
+  get '/admin/pictures/add' => 'admin#pictures_new'
+  post '/admin/pictures/add' => 'admin#pictures_create'
+  get '/admin/events/all' => 'admin#events_view'
+  get '/admin/events/:id' => 'admin#event_view'
+  get '/admin/events/:id/edit' => 'admin#event_edit'
+  post '/admin/events/:id/edit' => 'admin#event_update'
+  post '/admin/events/:id/delete' => 'admin#event_delete'
 
   match '/404' => 'errors#file_not_found', via: :all
 end
